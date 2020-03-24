5c6d5d5a0f4f38a1e966b8eed99c93b3758c175e
luka <lka.v.lv@gmail.com>
2020-03-24 11:45:15 +0100

    todo

---
+++ public/app.js
@@ -0,0 +1,43 @@
+const List = class {
+	constructor() {
+		// doms
+		this.$form = $('#itemForm');
+		this.$input = $('#item');
+		this.$container = $('.list');
+		this.init();
+	}
+
+	init() {
+		// laod items
+		// add event handlers
+	}
+
+	add(e) {
+		// get text from input and create item
+	}
+
+	show() {
+		// fetch('')
+		// 	.then(response => response.json())
+		// 	.then(data => {
+		// 	})
+		// 	.catch(err => console.log(err));
+	}
+
+	append(item) {}
+
+	save(item) {
+		// fetch('', {
+		// 	method: 'post',
+		// 	body: data,
+		// 	headers: {
+		// 		'Content-Type': 'application/json'
+		// 	}
+		// })
+		// 	.then(response => response.json())
+		// 	.then(data => {})
+		// 	.catch(err => console.log(err));
+	}
+};
+
+new List();
---
+++ server.js
@@ -0,0 +1,6 @@
+const fs = require('fs');
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+app.use(express.json());
+// app.use(express.urlencoded({ extended: false }));
